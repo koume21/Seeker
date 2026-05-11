48ce05be02281b51c8620fe37ac8a682f310b3ba
koumei <t.koumei.0121@gmail.com>
2026-05-11 19:25:17 +0900

    ホーム画面 名前とアイコン表示

--- seeker/src/proxy.ts
+++ seeker/src/proxy.ts
@@ -15,11 +15,11 @@ export default auth((req) => {
 
   // 3. ログイン済みでログインページにアクセスしたらトップへ戻す（任意）
   if (isLoggedIn && isLoginPage) {
-    return Response.redirect(new URL("/", nextUrl));
+    return Response.redirect(new URL("/main/home", nextUrl));
   }
 })
 
 // 適用範囲の設定（静的ファイルやAPIを除外）
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|resister).*)"],
 }
