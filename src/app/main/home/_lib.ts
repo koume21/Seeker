b6bf5a7bbcae2ad88a34a79cc3a0ac1d74740e66
koumei <t.koumei.0121@gmail.com>
2026-08-08 13:53:07 +0900

    ホーム画面へのcursor実装

--- src/app/main/home/_lib.ts
+++ src/app/main/home/_lib.ts
@@ -1,10 +1,13 @@
 import { prisma } from '@/lib/prisma';
 import { auth } from '@/auth';
 
-export const getPosts = async ( langName?: string,searchKey?: string) => {
+// 一度の表示ポスト数
+const PAGE_SIZE = 10;
+
+export const getPosts = async ( language_name?: string,search_contents?: string,cursor?:number) => {
     const session = await auth();
     if(!session?.user?.id) {
-        return [];
+        return { posts: [], nextCursor: null};
     }
 
     const userId = session.user.id;
@@ -12,18 +15,18 @@ export const getPosts = async ( langName?: string,searchKey?: string) => {
         userId: userId,
     };
 
-    if (langName) {
+    if (language_name) {
         const language = await prisma.language.findUnique({
-            where: { name: langName },
+            where: { name: language_name },
             select: { id: true }
         });
         
-        if (!language) return []; // 存在しない言語なら空配列を返す
+        if (!language) return { posts: [], nextCursor: null}; // 存在しない言語なら空配列を返す
         
         whereClause.languageId = language.id;
     }
-    if (searchKey && searchKey.trim()) {
-        const words = searchKey.replace(/\s+/g, ' ').trim().split(' ');
+    if (search_contents && search_contents.trim()) {
+        const words = search_contents.replace(/\s+/g, ' ').trim().split(' ');
         
         whereClause.AND = words.map((word) => ({
             OR: [
@@ -33,7 +36,7 @@ export const getPosts = async ( langName?: string,searchKey?: string) => {
         }));
     }
 
-    return await prisma.post.findMany({
+    const search_posts = await prisma.post.findMany({
         where: whereClause,
         select: {
             id: true,
@@ -46,6 +49,24 @@ export const getPosts = async ( langName?: string,searchKey?: string) => {
                 select: { id: true }
             } : false
         },
-        orderBy: { created_at: 'desc' },
+         orderBy: [{ created_at: 'desc' }, { id: 'desc' }],
+        take: PAGE_SIZE + 1,
+        ...(cursor ? {cursor: {id: cursor}, skip: 1}: {})
     });
+    // search_psotsが11件以上の判定
+    const hasMore = search_posts.length > PAGE_SIZE;
+
+    // hasMoreがtrueの場合10件のみpageRowsへ保存
+    const pageRows = hasMore ? search_posts.slice(0,PAGE_SIZE):search_posts;
+
+    //hasMoreがtrueの場合pageRowsの末尾のpost.idをnextCursorへ保存
+    const nextCursor = hasMore ? pageRows[pageRows.length - 1].id : null;
+
+    const posts = pageRows.map((post) => ({
+        ...post,
+        isLiked: post.likes ? post.likes.length > 0 : false,
+        likeCount: post.likes ? post.likes.length : 0 // いいね数を表示するために追加
+    }));
+
+    return {posts,nextCursor}
 }
